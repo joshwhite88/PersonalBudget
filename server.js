b4975bcbac7652d7b2c4d897ae14b10c27cfa1d6
Josh <143891382+joshwhite88@users.noreply.github.com>
2024-04-26 17:18:58 -0500

    don't create if envelope already exists

--- server.js
+++ server.js
@@ -12,25 +12,38 @@ budget: number,
 title: string 
 */
 
+const validate = function(req, res, next) {
+
+};
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
     res.send('Hello World');
 });
 
+app.get('/envelopes', (req, res, next) => {
+    res.send(envelopes);
+});
+
 app.post('/envelopes', (req, res, next) => {
-    console.log(req.body);
-    let id = envelopes.length + 1;
-    envelopes.push({envelopeId: id, budget: req.body.budget, title: req.body.title});
-    if (envelopes.find(envelope => envelope.envelopeId === id)) {
-        console.log('envelope added!');
-        console.log(envelopes);
-        res.status(201).send();
-    } else {
-        let err = new Error('envelope not added');
-        err.status(400);
+    let found = envelopes.find(envelope => envelope.title === req.body.title);
+    if (!found) {
+        let id = envelopes.length + 1;
+        envelopes.push({envelopeId: id, budget: req.body.budget, title: req.body.title});
+        let envelopeIndex = envelopes.findIndex(envelope => envelope.envelopeId === id);
+        res.status(201).send(envelopes[envelopeIndex]);
+    } 
+    else {
+        let errorMessage = new Error('envelope not added');
+        errorMessage.status = 400;
+        next(errorMessage);
     }
 });
 
+app.use((err, req, res, next) => {
+    let status = err.status || 400;
+    res.status(status).send(err.errorMessage);
+});
 
 app.listen(PORT);
