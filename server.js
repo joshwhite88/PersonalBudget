a59003b7dd854f970572468da1149f2e17532498
Josh <143891382+joshwhite88@users.noreply.github.com>
2024-04-28 08:44:28 -0500

    refactor post route, add delete route

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const envelopes = [];
 let budgetAvailable = 4350;
 let envelopeCounter = 1;
 
-// const adjustBudget = function(amount) {
-//     totalBudget -= amount;
-// };
-
 app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
@@ -30,7 +26,12 @@ app.param('envelopeId', (req, res, next, envelopeId) => {
 });
 
 const addEnvelope = function(req, res, next) {
-    let exists = envelopes.find(envelope => envelope.title === req.body.title);
+    if (!req.body.title || isNaN(req.body.budget)) {
+        let errorMessage = new Error('Missing required information');
+        return next(errorMessage);
+    }
+    let title = req.body.title.toLowerCase();
+    let exists = envelopes.find(envelope => envelope.title === title);
     if (exists) {
         let errorMessage = new Error(`Budget has already been allocated for ${req.body.title}`);
         return next(errorMessage);
@@ -40,30 +41,26 @@ const addEnvelope = function(req, res, next) {
         return next(errorMessage);
     }
     budgetAvailable -= req.body.budget;
+    envelopes.push(
+        {"envelopeId": envelopeCounter++,
+         "budget": req.body.budget,
+         "title": title
+        });
     next();
 };
 
 const updateEnvelope = function(req, res, next) {
+    let currentTitle = envelopes[req.index].title;
     let currentBudget = envelopes[req.index].budget;
     let adjustedBudget = req.body.budget - currentBudget;
     if (adjustedBudget > budgetAvailable || req.body.budget < 0) {
         let errorMessage = new Error('Invalid amount');
         return next(errorMessage);
     }
+    envelopes[req.index].title = req.body.title || currentTitle;
     envelopes[req.index].budget = req.body.budget;
     budgetAvailable -= adjustedBudget;
-    console.log(budgetAvailable);
     next();
-    // let currentBudget = envelopes[req.index].budget;
-    // console.log(currentBudget);
-    // let adjustedBudget = Math.abs(currentBudget - req.body.budget);
-    // console.log(adjustedBudget);
-    // if (adjustedBudget > budgetAvailable) {
-    //     let errorMessage = new Error('Adjusted budget would exceed total budget');
-    //     return next(errorMessage);
-    // }
-    // envelopes[req.index].budget = req.body.budget >= 0 ? req.body.budget : currentBudget;
-    // budgetAvailable -= adjustedBudget;
 };
 
 app.get('/envelopes', (req, res, next) => {
@@ -80,13 +77,23 @@ app.get('/envelopes/:envelopeId', (req, res, next) => {
 });
 
 app.post('/envelopes', addEnvelope, (req, res, next) => {
-    envelopes.push({"envelopeId": envelopeCounter++, "budget": req.body.budget, "title": req.body.title});
     res.status(201).send(envelopes[envelopes.length - 1]);
 });
 
 app.put('/envelopes/:envelopeId', updateEnvelope, (req, res, next) => {
     res.status(200).send(envelopes[req.index]);
 });
+
+app.delete('/envelopes/:title', (req, res, next) => {
+    let index = envelopes.findIndex(envelope => envelope.title === req.params.title.toLowerCase());
+    if (index >= 0) {
+        envelopes.splice(index, 1);
+        res.status(200).send(envelopes);
+    } else {
+        let errorMessage = new Error(`no budget found for ${req.params.title}`);
+        next(errorMessage);
+    }
+})
 
 app.use((err, req, res, next) => {
     let status = err.status || 400;
